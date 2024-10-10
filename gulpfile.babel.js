af1131522085c2fc2606bdc97f0891db07657989
kikeestrada <kikeestradadev@gmail.com>
2024-10-10 09:32:58 -0600

    adjustmets file.json multiples

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,21 +14,37 @@ import buffer from 'vinyl-buffer';
 import minify from 'gulp-minify';
 import data from 'gulp-data';
 import fs from 'fs';
+import path from 'path';
 import cacheBust from 'gulp-cache-bust';
 
 // Set the Sass compiler
 const sass = gulpSass(sassCompiler);
 
+// Función para leer todos los archivos JSON en un directorio
+const getJsonData = () => {
+    const dataDir = './src/data/';
+    const files = fs.readdirSync(dataDir);
+    let jsonData = {};
+
+    files.forEach(file => {
+        if (path.extname(file) === '.json') {
+            const fileData = fs.readFileSync(path.join(dataDir, file));
+            Object.assign(jsonData, JSON.parse(fileData));
+        }
+    });
+
+    return jsonData;
+};
+
+// Tarea pug
 gulp.task('pug', () => {
     return gulp.src('./src/pug/pages/**/*.pug')
         .pipe(plumber())
-        .pipe(data(() => {
-            return JSON.parse(fs.readFileSync('./src/data/example.json'));
-        }))
+        .pipe(data(() => getJsonData()))  // Usamos la función para obtener datos
         .pipe(pug({
-            pretty: true  // Esta opción asegura que el HTML no esté minificado
+            pretty: true  // Asegura que el HTML no esté minificado
         }))
-		.pipe(cacheBust({  // Aplicamos cache busting aquí
+        .pipe(cacheBust({  // Aplicamos cache busting aquí
             type: 'timestamp'  // Usamos timestamp para asegurar la actualización de cache
         }))
         .pipe(gulp.dest('public'));
@@ -71,7 +87,7 @@ gulp.task('serve', gulp.series('pug', 'sass', 'scripts', () => {
     gulp.watch('src/pug/**/*.pug', gulp.series('pug')).on('change', browserSync.reload);
     gulp.watch('src/scss/**/*.scss', gulp.series('sass')).on('change', browserSync.reload);
     gulp.watch('src/js/**/*.js', gulp.series('scripts')).on('change', browserSync.reload);
-    gulp.watch('src/data/example.json', gulp.series('pug')).on('change', browserSync.reload);
+    gulp.watch('src/data/**/*.json', gulp.series('pug')).on('change', browserSync.reload);
     gulp.watch('src/md/**/*.md', gulp.series('pug')).on('change', browserSync.reload); // Add this line
 }));
 
