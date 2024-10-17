69a512509d92344464f7784eedf321984b556672
kikeestrada <kikeestradadev@gmail.com>
2024-10-17 14:22:06 -0600

    Updates

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 
 var _internalModule = _interopRequireDefault(require("./modules/internalModule"));
 var _tutorialNavSlider = _interopRequireDefault(require("./modules/tutorialNavSlider"));
+var _tutorialCategorySlider = _interopRequireDefault(require("./modules/tutorialCategorySlider"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 (function () {
   (0, _internalModule["default"])();
   (0, _tutorialNavSlider["default"])();
+  (0, _tutorialCategorySlider["default"])();
 })();
 
-},{"./modules/internalModule":2,"./modules/tutorialNavSlider":3}],2:[function(require,module,exports){
+},{"./modules/internalModule":2,"./modules/tutorialCategorySlider":3,"./modules/tutorialNavSlider":4}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -22,6 +24,38 @@ var internalModule = function internalModule() {
 var _default = exports["default"] = internalModule;
 
 },{}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var tutorialCategorySlider = function tutorialCategorySlider() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var tutorialCategorySliderItem = document.querySelector('.tutorial-category-slider');
+    if (tutorialCategorySliderItem) {
+      var _tutorialCategorySlider = new Swiper('.tutorial-category-slider', _defineProperty(_defineProperty({
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        allowThresholdMove: true,
+        slidesPerView: 'auto',
+        spaceBetween: 0
+      }, "spaceBetween", 7), "a11y", {
+        enabled: true,
+        slideLabelMessage: "Slide {{index}} of {{slidesLength}}",
+        slideRole: null // Custom role for slide
+      }));
+    }
+  });
+};
+var _default = exports["default"] = tutorialCategorySlider;
+
+},{}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
